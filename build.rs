d9319fb26a0fa6353d49ada21b8a68028c0d30d0
Mike Hommey <mh@glandium.org>
2021-09-15 18:37:56 +0900

    Do a more fine-grained test for versions of 1.56 nightly that still support const_generics

--- build.rs
+++ build.rs
@@ -1,4 +1,4 @@
-use rustc_version::{version, Version};
+use rustc_version::{version_meta, Channel, Version};
 
 fn main() {
     println!("cargo:rustc-env=RUSTC_BOOTSTRAP=1");
@@ -7,8 +7,25 @@ fn main() {
     if target.contains("neon") {
         println!("cargo:rustc-cfg=libcore_neon");
     }
-    let ver = version().unwrap();
-    if ver < Version::parse("1.56.0-alpha").unwrap() {
+    let ver_meta = version_meta().unwrap();
+    let old_const_generics = match dbg!(ver_meta.channel) {
+        Channel::Stable | Channel::Beta
+            if ver_meta.semver < Version::parse("1.56.0-alpha").unwrap() =>
+        {
+            true
+        }
+        Channel::Nightly | Channel::Dev
+            if ver_meta
+                .commit_date
+                .as_deref()
+                .map(|d| d < "2021-08-31")
+                .unwrap_or(false) =>
+        {
+            true
+        }
+        _ => false,
+    };
+    if old_const_generics {
         println!("cargo:rustc-cfg=const_generics");
     }
 }
